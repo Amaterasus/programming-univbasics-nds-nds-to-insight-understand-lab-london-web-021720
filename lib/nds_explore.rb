52070bda153d98beed75bef88997d0b1d443875c
Skye Geraghty <skyetay03@googlemail.com>
2020-02-06 04:20:13 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,14 +8,12 @@ def pretty_print_nds(nds)
   pp nds
 end
 
-pretty_print_nds(directors_database)
-
 def print_first_directors_movie_titles
   movies = directors_database[0][:movies]
   index = 0
   
   while index < movies.length do
-    
+    puts movies[index][:title]
     index ++
   end
 end
